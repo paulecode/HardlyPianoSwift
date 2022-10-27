78a8a840f36e1be735ed78693931b218cfcbf584
Paul Ebert <paul.ebert@code.berlin>
2022-10-27 16:41:19 +0200

    Initial Commit

---
+++ HardlyPianoSwift/HardlyPianoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardlyPianoSwiftApp.swift
+//  HardlyPianoSwift
+//
+//  Created by Paul on 27.10.22.
+//
+
+import SwiftUI
+
+@main
+struct HardlyPianoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
