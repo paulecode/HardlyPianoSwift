8b38adf152dd0e11491c8d3691799dffff31cfd5
Paul Ebert <paul.ebert@code.berlin>
2022-11-03 21:47:12 +0000

    Some mainview content

---
+++ HardlyPianoSwift/Sections/MainView/HomeView.swift
@@ -0,0 +1,34 @@
+//
+//  HomeView.swift
+//  HardlyPianoSwift
+//
+//  Created by Paul on 03.11.22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+		VStack {
+			HStack {
+				Text("Top Bar")
+				Image(systemName: "gearshape.fill")
+			}
+			//Replace with top bar
+			Text("Continue practicing")
+			Text("This is where you left off")
+			PieceViewSpecial()
+			Text("Refresh memory")
+			Text("You haven't played this one in a while")
+			PieceViewSpecial()
+			Text("All my pieces")
+			
+		}
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
