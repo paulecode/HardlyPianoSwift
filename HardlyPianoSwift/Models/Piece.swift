d5c40babc280c7c73a3724fb1c7fa658c9e6de40
Paul Ebert <paul.ebert@code.berlin>
2022-11-04 22:05:19 +0000

    Simple Piece Model

---
+++ HardlyPianoSwift/Models/Piece.swift
@@ -0,0 +1,15 @@
+//
+//  Piece.swift
+//  HardlyPianoSwift
+//
+//  Created by Paul on 04.11.22.
+//
+
+import Foundation
+
+struct Piece {
+	let id: Int
+	let title: String
+	let composer: String
+	let practiceTime: Int
+}
