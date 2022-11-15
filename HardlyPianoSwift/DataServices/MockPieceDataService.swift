dd90601c799254f161581fe28bfa6fd90bda7487
Paul Ebert <paul.ebert@code.berlin>
2022-11-15 14:31:17 +0100

    Fix: mutate piece struct directly in PieceService.updatePiece.

--- HardlyPianoSwift/DataServices/MockPieceDataService.swift
+++ HardlyPianoSwift/DataServices/MockPieceDataService.swift
@@ -14,58 +14,58 @@ protocol pieceServiceProtocol {
 	func getAllPieces() async throws -> [Piece]
 	
 	func postPiece(title: String, composer: String) async throws -> Piece
-
+	
 	func deletePiece(piece: Piece) async throws -> Bool
-
+	
 	func updatePiece(oldPiece: Piece, title: String, composer: String) async throws -> Piece
 	
 }
 
 class MockPieces: ObservableObject, pieceServiceProtocol {
 	
-	var token: String 
+	var token: String
 	
 	init() {
 		self.token = "validtoken"
 	}
 	
 	@Published var pieces: [Piece] = [
-	Piece(
-		mongoID: "632b4856fa42ef9537d9b375",
-		title: "Moonlight Sonata Mov. 1",
-		composer: "Beethoven",
-		practiceTime: 6
-	),
-	Piece(
-		mongoID: "632b4860fa42ef9537d9b377",
-		title: "Moonlight Sonata Mov. 2",
-		composer: "Beethoven",
-		practiceTime: 9
-	),
-	Piece(
-		mongoID: "632b4868fa42ef9537d9b379",
-		title: "Moonlight Sonata Mov. 3",
-		composer: "Beethoven",
-		practiceTime: 12
-	),
-	Piece(
-		mongoID: "632b48b7fa42ef9537d9b385",
-		title: "Lacrimosa",
-		composer: "Mozart",
-		practiceTime: 7
-	),
-	Piece(
-		mongoID: "632b4910fa42ef9537d9b38b",
-		title: "Nocturne Opus 9 Nr. 2",
-		composer: "Chopin",
-		practiceTime: 25
-	),
-	Piece(
-		mongoID: "632b4917fa42ef9537d9b38d",
-		title: "Nocturne Opus 9 Nr. 1",
-		composer: "Chopin",
-		practiceTime: 1
-	),
+		Piece(
+			mongoID: "632b4856fa42ef9537d9b375",
+			title: "Moonlight Sonata Mov. 1",
+			composer: "Beethoven",
+			practiceTime: 6
+		),
+		Piece(
+			mongoID: "632b4860fa42ef9537d9b377",
+			title: "Moonlight Sonata Mov. 2",
+			composer: "Beethoven",
+			practiceTime: 9
+		),
+		Piece(
+			mongoID: "632b4868fa42ef9537d9b379",
+			title: "Moonlight Sonata Mov. 3",
+			composer: "Beethoven",
+			practiceTime: 12
+		),
+		Piece(
+			mongoID: "632b48b7fa42ef9537d9b385",
+			title: "Lacrimosa",
+			composer: "Mozart",
+			practiceTime: 7
+		),
+		Piece(
+			mongoID: "632b4910fa42ef9537d9b38b",
+			title: "Nocturne Opus 9 Nr. 2",
+			composer: "Chopin",
+			practiceTime: 25
+		),
+		Piece(
+			mongoID: "632b4917fa42ef9537d9b38d",
+			title: "Nocturne Opus 9 Nr. 1",
+			composer: "Chopin",
+			practiceTime: 1
+		),
 	]
 	
 	func setToken(token: String) {
@@ -89,10 +89,24 @@ class MockPieces: ObservableObject, pieceServiceProtocol {
 	
 	func updatePiece(oldPiece: Piece, title: String, composer: String) async throws -> Piece {
 		//Check what changed
-		let newPiece = Piece(mongoID: oldPiece.mongoID, title: title, composer: composer, practiceTime: oldPiece.practiceTime)
-//		
-//		pieces = pieces.filter({$0 != oldPiece})
-//		pieces.append(newPiece)
-		return newPiece
+//		let newPiece = Piece(mongoID: oldPiece.mongoID, title: title, composer: composer, practiceTime: oldPiece.practiceTime) //
+		//		pieces = pieces.filter({$0 != oldPiece})
+		//		pieces.append(newPiece)
+		guard let index = self.pieces.firstIndex(where: {$0.mongoID == oldPiece.mongoID} ) else {
+			throw RequestError.BadRequest
+		}
+		pieces[index].title = title
+		pieces[index].composer = composer
+//		let piece = pieces[index]
+//		piece.title = title
+//		piece.composer = composer
+		return pieces[index]
+		
+//		print("got here", index)
+//		print("Before", pieces[index])
+//		print("updated", updatedPiece)
+//		pieces[index] = updatedPiece
+//		print("Updated array", pieces)
+//		return newPiece
 	}
 }
